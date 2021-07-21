4a3480f828724c83afaefd5829aa094ffe27427c
mahdi <mahdi.kashanipour@gmail.com>
2021-07-21 00:19:23 -0400

    header link style
    Pure creativity :)

--- component/header.js
+++ component/header.js
@@ -1,16 +1,27 @@
 import Link from "next/link";
 
+const headerStyle = {
+  marginRight: 2,
+  backgroundColor: "",
+  borderRadius: "10px",
+  fontFamily: "Arial",
+  textDecoration: "none",
+  boxShadow: "3px 3px",
+  margin: "8px 8px",
+  padding: "2px 8px",
+};
+
 const Header = () => {
   return (
     <div>
       <Link href="/about">
-        <a>about </a>
+        <a style={headerStyle}>About</a>
       </Link>
       <Link href="/blog">
-        <a>blog </a>
+        <a style={headerStyle}>Blog</a>
       </Link>
       <Link href="/">
-        <a>index </a>
+        <a style={headerStyle}>Index</a>
       </Link>
       <hr></hr>
     </div>
